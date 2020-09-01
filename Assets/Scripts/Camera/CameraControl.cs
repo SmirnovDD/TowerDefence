9905d3e54f6487459946f01d3265f5402fd74b8d
SmirnovDD <studioindevelopment@gmail.com>
2020-09-01 22:15:21 +0300

    Struggling with the camera

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using UnityEngine;
-using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
-	public float Speed = 5;
+	public float Speed = 12f;
 
 	//public KeyCode Left = KeyCode.A;
 	//public KeyCode Right = KeyCode.D;
@@ -15,51 +14,71 @@ public class CameraControl : MonoBehaviour {
 	public KeyCode ResetCameraRotation = KeyCode.R;
 	
 	public float RotationX;
-	public float MaxHeight = 15;
-	public float MinHeight = 5;
+	public float MaxHeight;
+	public float MinHeight;
 
-	private float _rotationY;
+	public float YAngleAtBottomPosition = 25f;
+	public float ZoomSpeed;
+	
+	private float _initialRotationY;
 	private float _height;
-
-
+	private float _zoomLerpTime;
+	private Vector2 _zoomDirection;
+	
 	private void Start()
 	{
 		_height = transform.position.y;
-		_rotationY = transform.rotation.eulerAngles.y;
+		_zoomLerpTime = (_height - MinHeight) / (MaxHeight - MinHeight);
+		_initialRotationY = transform.rotation.eulerAngles.y;
 		RotationX = transform.rotation.eulerAngles.x;
 	}
 
 	void Update ()
 	{
-		MoveCamera();
-		
+		MoveCameraWithKeyboard();
+		ZoomCamera();
+		ChangeCameraAngle();
 		if(Input.GetKey(RotateCameraRight)) 
-			_rotationY -= 3;
+			_initialRotationY -= 3;
 		else if(Input.GetKey(RotateCameraLeft)) 
-			_rotationY += 3;
-
-		var oldHeight = _height;
-		
-		if(Input.GetAxis("Mouse ScrollWheel") < 0)
-		{
-			if(_height < MaxHeight) 
-				_height += 1;
-		}
-		if(Input.GetAxis("Mouse ScrollWheel") > 0)
-		{
-			if(_height > MinHeight) 
-				_height -= 1;
-		}
-
-		_height = Mathf.Lerp(oldHeight, _height, 3 * Time.deltaTime);
+			_initialRotationY += 3;
 
 		transform.position = new Vector3(transform.position.x, _height, transform.position.z);
-		transform.rotation = Quaternion.Euler(RotationX, _rotationY, 0);
+		transform.rotation = Quaternion.Euler(RotationX, _initialRotationY, 0);
 	}
 
-	private void MoveCamera()
+	private void MoveCameraWithKeyboard()
 	{
 		Vector3 moveVector = Vector3.right * Input.GetAxis("Horizontal") + Vector3.forward * Input.GetAxis("Vertical");
 		transform.Translate(moveVector * Speed * Time.deltaTime);
+	}
+
+	private void ZoomCamera()
+	{
+		
+		if(Input.GetAxis("Mouse ScrollWheel") < 0)
+		{
+			MoveCameraVertically(Vector2.up);
+		}
+		else if(Input.GetAxis("Mouse ScrollWheel") > 0)
+		{
+			MoveCameraVertically(Vector2.down);
+		}
+		
+		_height = Mathf.SmoothStep(MinHeight, MaxHeight, _zoomLerpTime);
+	}
+
+	private void MoveCameraVertically(Vector2 dir)
+	{
+		if(_zoomDirection != dir || _zoomLerpTime >= 1)
+			_zoomLerpTime = (_height - MinHeight) / (MaxHeight - MinHeight);
+			
+		_zoomLerpTime += Time.deltaTime * Input.GetAxis("Mouse ScrollWheel") * ZoomSpeed;
+		_zoomDirection = dir;
+	}
+	private void ChangeCameraAngle()
+	{
+		//Debug.Log(_height/(MaxHeight-MinHeight));
+		//	RotationX = Mathf.Lerp(RotationX, YAngleAtBottomPosition, _initialRotationY, (MaxHeight  / MinHeight))
 	}
 }
