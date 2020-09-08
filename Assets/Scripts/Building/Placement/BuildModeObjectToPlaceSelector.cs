1c5d1f0aabb40e8b1be9e3575097f0e680e10308
SmirnovDD <studioindevelopment@gmail.com>
2020-09-08 21:04:26 +0300

    added buttons

---
+++ Assets/Scripts/Building/Placement/BuildModeObjectToPlaceSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildModeObjectToPlaceSelector : MonoBehaviour
+{
+    public ObjectPlacement ObjectPlacement;
+    public void SelectObjectToPlace(GameObject objectToPlace)
+    {
+        Debug.Log(objectToPlace.name);
+        ObjectPlacement.ChangePlacementObject(objectToPlace);
+    }
+}
