4fac8a1a6272e844de68f621fe560b6fb7ee653f
SmirnovDD <studioindevelopment@gmail.com>
2020-09-14 21:07:42 +0300

    added pathfinding unit + animator controller

---
+++ Assets/Scripts/Animations/UnitAnimationController.cs
@@ -0,0 +1,212 @@
+﻿using UnityEngine;
+public class UnitAnimationController : MonoBehaviour
+{
+    public Animator Anim;
+
+    private const string LowerBodyClipName = "LowerBody";
+    private const string UpperBodyClipName = "UpperBody";
+    private const string FullBodyClipName = "FullBody";
+
+    private static readonly int _startAnimLowerBodyTrigger = Animator.StringToHash("StartAnimLowerBody");
+    private static readonly int _stopAnimLowerBodyTrigger = Animator.StringToHash("StopAnimLowerBody");
+    
+    private static readonly int _startAnimUpperBodyTrigger = Animator.StringToHash("StartAnimUpperBody");
+    private static readonly int _stopAnimUpperBodyTrigger = Animator.StringToHash("StopAnimUpperBody");
+
+    private static readonly int _startAnimFullBodyTrigger = Animator.StringToHash("StartAnimFullBody");
+    private static readonly int _stopAnimFullBodyTrigger = Animator.StringToHash("StopAnimFullBody");
+
+    private AnimationClip _lowerBodyPreviousClip;
+    private AnimationClip _upperBodyPreviousClip;
+
+    private float _lowerBodyAnimationTimer;
+    private float _upperBodyAnimationTimer;
+    private float _fullBodyAnimationTimer;
+
+    private bool _stopLowerBodyAnimation;
+    private bool _stopUpperBodyAnimation;
+    private bool _stopFullBodyAnimation;
+    
+    public enum AnimationBodyPart
+    {
+        LowerBody,
+        UpperBody,
+        FullBody
+    }
+    private void Update()
+    {
+        if (GameTimeManager.Me.GameIsPaused)
+            Anim.speed = 0;
+        else
+            Anim.speed = 1;
+        
+        UpdateLowerBodyAnimationTimer();
+
+        UpdateUpperBodyAnimationTimer();
+
+        UpdateFullBodyAnimationTimer();
+    }
+
+    private void UpdateLowerBodyAnimationTimer()
+    {
+        if (_lowerBodyAnimationTimer > 0 && _stopLowerBodyAnimation)
+            _lowerBodyAnimationTimer -= GameTimeManager.Me.DeltaTime;
+        else if (_lowerBodyAnimationTimer <= 0 && _stopLowerBodyAnimation)
+            StopAnimationClip(AnimationBodyPart.LowerBody);
+    }
+
+    private void UpdateUpperBodyAnimationTimer()
+    {
+        if (_upperBodyAnimationTimer > 0 && _stopUpperBodyAnimation)
+            _upperBodyAnimationTimer -= GameTimeManager.Me.DeltaTime;
+        else if (_upperBodyAnimationTimer <= 0 && _stopUpperBodyAnimation)
+            StopAnimationClip(AnimationBodyPart.UpperBody);
+    }
+
+    private void UpdateFullBodyAnimationTimer()
+    {
+        if (_fullBodyAnimationTimer > 0 && _stopFullBodyAnimation)
+            _fullBodyAnimationTimer -= GameTimeManager.Me.DeltaTime;
+        else if (_fullBodyAnimationTimer <= 0 && _stopFullBodyAnimation)
+            StopAnimationClip(AnimationBodyPart.FullBody);
+    }
+    
+    public void ChangeAnimationClip(AnimationClip clip, AnimationBodyPart bodyPart)
+    {
+        RuntimeAnimatorController myController = Anim.runtimeAnimatorController;
+
+        AnimatorOverrideController myOverrideController = myController as AnimatorOverrideController;
+        if(myOverrideController != null)
+            myController = myOverrideController.runtimeAnimatorController;
+
+        AnimatorOverrideController animatorOverride = new AnimatorOverrideController
+        {
+            runtimeAnimatorController = myController
+        };
+        
+        var clipOverrides = new AnimationClipOverrides(animatorOverride.overridesCount);
+
+        UpdateAnimationClipsOnOtherLayers(bodyPart, animatorOverride, clip);
+        
+        animatorOverride.ApplyOverrides(clipOverrides);
+        Anim.runtimeAnimatorController = animatorOverride;
+
+        SetTriggerToStartAnimation(bodyPart);
+        
+        if (clip.isLooping == false)
+            StartTimerForNonLoopedAnimationClip(bodyPart, clip);
+        else
+            StopTimerForLoopedAnimationClip(bodyPart);
+    }
+
+    private void UpdateAnimationClipsOnOtherLayers(AnimationBodyPart bodyPart, AnimatorOverrideController animatorOverride, AnimationClip clip)
+    {
+        if (bodyPart == AnimationBodyPart.LowerBody)
+        {
+            UpdateLowerBodyAnimationClip(animatorOverride, clip);
+        }
+        else if (bodyPart == AnimationBodyPart.UpperBody)
+        {
+            UpdateUpperBodyAnimationClip(animatorOverride, clip);
+        }
+        else if (bodyPart == AnimationBodyPart.FullBody)
+        {
+            UpdateFullBodyAnimationClip(animatorOverride, clip);
+        }
+    }
+
+    private void UpdateLowerBodyAnimationClip(AnimatorOverrideController animatorOverride, AnimationClip clip)
+    {
+        animatorOverride[LowerBodyClipName] = clip;
+        animatorOverride[UpperBodyClipName] = _upperBodyPreviousClip;
+        StopAnimationClip(AnimationBodyPart.FullBody);
+        _lowerBodyPreviousClip = clip;
+    }
+    
+    private void UpdateUpperBodyAnimationClip(AnimatorOverrideController animatorOverride, AnimationClip clip)
+    {
+        animatorOverride[UpperBodyClipName] = clip;
+        animatorOverride[LowerBodyClipName] = _lowerBodyPreviousClip;
+        StopAnimationClip(AnimationBodyPart.FullBody);
+        _upperBodyPreviousClip = clip;
+    }
+
+    private void UpdateFullBodyAnimationClip(AnimatorOverrideController animatorOverride, AnimationClip clip)
+    {
+        animatorOverride[FullBodyClipName] = clip;
+        animatorOverride[LowerBodyClipName] = _lowerBodyPreviousClip;
+        animatorOverride[UpperBodyClipName] = _upperBodyPreviousClip;
+    }
+
+    private void SetTriggerToStartAnimation(AnimationBodyPart bodyPart)
+    {
+        if (bodyPart == AnimationBodyPart.LowerBody)
+        {
+            Anim.ResetTrigger(_stopAnimLowerBodyTrigger);
+            Anim.SetTrigger(_startAnimLowerBodyTrigger);
+        }
+        if (bodyPart == AnimationBodyPart.UpperBody)
+        {
+            Anim.ResetTrigger(_stopAnimUpperBodyTrigger);
+            Anim.SetTrigger(_startAnimUpperBodyTrigger);
+        }
+        if (bodyPart == AnimationBodyPart.FullBody)
+        {
+            Anim.ResetTrigger(_stopAnimFullBodyTrigger);
+            Anim.SetTrigger(_startAnimFullBodyTrigger);
+        }
+    }
+
+    private void StartTimerForNonLoopedAnimationClip(AnimationBodyPart bodyPart, AnimationClip clip)
+    {
+        if (bodyPart == AnimationBodyPart.LowerBody)
+        {
+            _lowerBodyAnimationTimer = clip.length;
+            _stopLowerBodyAnimation = true;
+        }
+
+        if (bodyPart == AnimationBodyPart.UpperBody)
+        {
+            _upperBodyAnimationTimer = clip.length;
+            _stopUpperBodyAnimation = true;
+        }
+        
+        if (bodyPart == AnimationBodyPart.FullBody)
+        {
+            _fullBodyAnimationTimer = clip.length;
+            _stopFullBodyAnimation = true;
+        }
+    }
+
+    private void StopTimerForLoopedAnimationClip(AnimationBodyPart bodyPart)
+    {
+        if (bodyPart == AnimationBodyPart.LowerBody)
+            _stopLowerBodyAnimation = false;
+        else if (bodyPart == AnimationBodyPart.UpperBody)
+            _stopUpperBodyAnimation = false;
+        else if (bodyPart == AnimationBodyPart.FullBody)
+            _stopFullBodyAnimation = false;
+    }
+    public void StopAnimationClip(AnimationBodyPart bodyPart)
+    {
+        if (bodyPart == AnimationBodyPart.LowerBody)
+        {
+            Anim.ResetTrigger(_startAnimLowerBodyTrigger);
+            Anim.SetTrigger(_stopAnimLowerBodyTrigger);
+        }
+
+        if (bodyPart == AnimationBodyPart.UpperBody)
+        {
+            Anim.ResetTrigger(_startAnimUpperBodyTrigger);
+            Anim.SetTrigger(_stopAnimUpperBodyTrigger);
+        }
+
+        if (bodyPart == AnimationBodyPart.FullBody)
+        {
+            Anim.ResetTrigger(_startAnimFullBodyTrigger);
+            Anim.SetTrigger(_stopAnimFullBodyTrigger);
+        }
+
+        StopTimerForLoopedAnimationClip(bodyPart);
+    }
+}
---
+++ Assets/Scripts/Pathfinding/PathfindingUnit.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.Collections;
+
+namespace Pathfinding
+{
+    //[RequireComponent(typeof(SailorModel))]
+    public class PathfindingUnit : MonoBehaviour
+    {
+        public Transform TestTarget;
+        [Header("Movement Parameters")]
+        public float BaseSpeed;
+        [ReadOnly] public float Speed;
+        public float TurnSpeed;
+
+        [Header("Status")]
+        [ReadOnly] public State MoveState;
+        [ReadOnly] public PathfindingGrid CurrentGrid;
+        [Header("Debug Display")]
+        [CanBeNull] public PathfindingManager.TargetPoint CurrentWaypoint;
+        [CanBeNull] public Node TargetNode;
+
+        private List<PathfindingManager.TargetPoint> Waypoints = new List<PathfindingManager.TargetPoint>();
+        private Coroutine _lerpToBehaviourCoroutine;
+        private Coroutine _connectionMovementCoroutine;
+        private bool _newPathAfterConnectionFinishes;
+        private PathfindingGrid newGrid;
+        [NonSerialized] public float SqrWaypointReachDistance;
+        private int _currentWaypointIndex;
+        private PathData _pathToWaypoint = new PathData(new Node[0], Vector3.zero);
+        private int _pathIndex = 0;
+
+        private Vector3 _lastLocalTargetPos;
+        private UnitAnimationController _animationController;
+
+        private void Awake()
+        {
+            _animationController = GetComponent<UnitAnimationController>();
+        }
+
+        public void Start()
+        {
+            MoveState = State.Stationary;
+            SqrWaypointReachDistance = PathfindingManager.Me.SqrWaypointReachDistance;
+        }
+
+        public void SetPath(Transform target)
+        {
+            if (Vector3.Distance(_lastLocalTargetPos, target.position)< 0.1f)
+                return;
+
+            _lastLocalTargetPos = target.position;
+
+            SetWaypoints(target);
+            RequestPathToWaypoint();            
+        }
+        
+        private void SetWaypoints(Transform target)
+        {
+            Waypoints = PathfindingManager.Me.CalculatePathForSailor(this, target);
+            CurrentWaypoint = Waypoints[0];
+            _currentWaypointIndex = 0;
+        }
+
+        private void RequestPathToWaypoint()
+        {
+            PathfindingMultithreading.RequestPath(new PathRequest(transform.position,
+                CurrentWaypoint.TargetPosition.position,
+                OnPathFound, CurrentGrid, true, true, BaseSpeed));
+        }
+
+        private void OnPathFound(Node[] nodes, bool pathSuccessful, float pathLength, float timeToFinish)
+        {
+            if (pathSuccessful == false)
+            {
+                Debug.Log("UNSUCCESSFUL PATH " + gameObject.name + " at " + transform.localPosition);
+                return;
+            }
+            InitPath(nodes, pathLength, timeToFinish);
+        }
+
+        private void InitPath(Node[] nodes, float pathLength, float timeToFinish)
+        {
+            if (nodes.Length == 0)
+                Debug.Log("Should never happen");
+
+            if (MoveState == State.LerpingIntoBehaviour)
+            {
+                StopCoroutine(_lerpToBehaviourCoroutine);
+                _lerpToBehaviourCoroutine = null;
+            }
+
+            //_sailorModel.ToggleAnimation(State.Stationary, false);
+            //_sailorModel.ToggleAnimation(State.Moving, true);
+            MoveState = State.Moving;
+            _pathToWaypoint = new PathData(nodes, transform.position);
+            _pathIndex = 0;
+        }
+
+        private void Update()
+        {
+            if(TestTarget)
+                SetPath(TestTarget);
+            if (MoveState == State.Moving)
+            {
+                TargetNode = _pathToWaypoint.Nodes[_pathIndex];
+                if (GetSqrDistanceToPoint(TargetNode.Position) > SqrWaypointReachDistance)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, TargetNode.Position, Speed * GameTimeManager.Me.DeltaTime);
+                    if (TargetNode.Position - transform.position != Vector3.zero)
+                    {
+                        Quaternion targetRotation = Quaternion.LookRotation (TargetNode.Position - transform.position);
+                        transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, GameTimeManager.Me.DeltaTime * TurnSpeed);
+                        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+                    }
+                }
+                else
+                {
+                    _pathIndex++;
+                    if (_pathIndex >= _pathToWaypoint.Nodes.Length)
+                    {
+                        OnWaypointReached();
+                    }
+                }
+            }
+        }
+        public void OnWaypointReached()
+        {
+            SetNextWaypoint();
+            if (CurrentWaypoint == null)
+            {
+                MoveState = State.LerpingIntoBehaviour;
+                _lerpToBehaviourCoroutine =
+                    StartCoroutine(LerpIntoBehaviour(_lastLocalTargetPos));
+            }
+            else
+            {
+                var previousWaypoint = Waypoints[_currentWaypointIndex - 1];
+                if (Vector3.SqrMagnitude(CurrentWaypoint.TargetPosition.position - previousWaypoint.TargetPosition.position) < SqrWaypointReachDistance)
+                {
+                    OnWaypointReached(); //if SetNextWaypoint will make currentwaypoint equal to null, we want to finish movement, we also want to check for connection special movement
+                }
+                else
+                {                                      
+                    RequestPathToWaypoint();                    
+                }
+            }
+        }
+
+        private void SetNextWaypoint()
+        {
+            _currentWaypointIndex++;
+            CurrentWaypoint = _currentWaypointIndex < Waypoints.Count ? Waypoints[_currentWaypointIndex] : null;
+            if (CurrentWaypoint != null)
+            {
+                //_sailorModel.ToggleAnimation(State.Moving, false);
+            }
+        }
+
+        private IEnumerator LerpIntoBehaviour(Vector3 pos)
+        {
+            float t = 0;
+            Vector3 initialPos = transform.position;
+            Quaternion initialRot = transform.rotation;
+            while (t < 1)
+            {
+                t += 2 * GameTimeManager.Me.DeltaTime;
+                transform.position = Vector3.Lerp(initialPos, pos, t);
+                transform.rotation = Quaternion.Lerp(initialRot, Quaternion.identity, t);
+                yield return null;
+            }
+            //_sailorModel.ToggleAnimation(State.Moving, false);
+            //_sailorModel.ToggleAnimation(State.Stationary, true);
+            MoveState = State.Stationary;
+            _lerpToBehaviourCoroutine = null;
+        }
+        private float GetSqrDistanceToPoint(Vector3 point)
+        {
+            return Vector3.SqrMagnitude(transform.position - point);
+        }
+    }
+
+    public enum State
+    {
+        Stationary,
+        Moving,
+        LerpingIntoBehaviour,
+        MovingThroughConnection
+    }
+}
